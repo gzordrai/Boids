d9a495cc3ce7672200e9771622e8c3f1d82386ce
gzordrai <40698142+gzordrai@users.noreply.github.com>
2022-11-24 22:51:12 +0100

    Add borders around the window

--- src/boid.py
+++ src/boid.py
@@ -1,18 +1,18 @@
 from numpy.random import random
 from p5 import *
 
-
 class Boid():
     def __init__(self, x, y, width, height):
-        self.position = Vector(x, y)
-
         vx = (random() - 0.5) * 5
         vy = (random() - 0.5) * 5
-        self.velocity = Vector(vx, vy)
-
         ax = (random() - 0.5) / 2
         ay = (random() - 0.5) / 2
+
+        self.position = Vector(x, y)
+        self.velocity = Vector(vx, vy)
         self.acceleration = Vector(ax, ay)
+        self.width = width
+        self.height = height
 
     def show(self):
         stroke(255)
@@ -21,3 +21,15 @@ class Boid():
     def update(self):
         self.position += self.velocity
         self.velocity += self.acceleration
+
+        # Left and right borders
+        if self.position.x > self.width:
+            self.position.x = 0
+        elif self.position.x < 0:
+            self.position.x = self.width
+
+        # Top and bottom borders
+        if self.position.y > self.height:
+            self.position.y = 0
+        elif self.position.y < 0:
+            self.position.y = self.height
